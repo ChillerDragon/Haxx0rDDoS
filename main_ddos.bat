f4a77c737fbe0fef6ef85f291abc08a714c96eb3
ChillerDragon <ChillerDragon@gmail.com>
2018-09-28 23:22:26 +0200

    Set main console position on start up

--- main_ddos.bat
+++ main_ddos.bat
@@ -2,10 +2,17 @@
 
 title main_ddos
 
+::init the proper position of the main console it sef so it wont be obverlapped by the ddos hud
+::cant be done from the window it self so call a helper file
+start set_main.bat
+cmdow\bin\Release\cmdow.exe set_main* /mov 1300 500
+
 :ddos
+@echo. > haxlog.txt
 cls
 set /p ip=ip: 
 echo ddosing %ip%...
+@echo %RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM% > haxlog.txt
 
 start dos_logo.bat
 cmdow\bin\Release\cmdow.exe dos_logo* /mov 500 1
@@ -18,7 +25,6 @@ start dos_bot3.bat :: center ip spam
 cmdow\bin\Release\cmdow.exe dos_bot3* /mov 500 170
 start dos_bot4.bat :: right loading + offset
 cmdow\bin\Release\cmdow.exe dos_bot4* /mov 1600 10
-
 
 pause>nul
 goto ddos
---
+++ set_main.bat
@@ -0,0 +1,12 @@
+@echo off
+
+title set_main
+
+::init the proper position so it wont be obverlapped by the ddos hud
+cmdow\bin\Release\cmdow.exe main_ddos* /mov 500 500
+
+:haxx0r_nums
+
+type haxlog.txt
+
+goto haxx0r_nums
